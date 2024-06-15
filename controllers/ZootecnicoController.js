112ddd4403b9f4b3d056f601377b204990d8a66c
jaobarreto <joaobarretoprof@gmail.com>
2024-06-15 18:15:35 -0300

    arrumando grafico

--- controllers/ZootecnicoController.js
+++ controllers/ZootecnicoController.js
@@ -1,35 +1,33 @@
-import express from 'express';
-const router = express.Router();
-import Bufalo from '../models/Bufalo.js';
-import Zootecnico from '../models/Zootecnico.js';
+import express from 'express'
+const router = express.Router()
+import Bufalo from '../models/Bufalo.js'
+import Zootecnico from '../models/Zootecnico.js'
 
-router.get('/zootecnico', (req, res) => {
-    const bufalosPromise = Bufalo.findAll(); // Buscar todos os búfalos cadastrados
-    const ultimoBufaloPromise = Bufalo.findOne({ order: [['id', 'DESC']] }); // Buscar último búfalo cadastrado
+router.get('/zootecnico', async (req, res) => {
+    try {
+        const bufalos = await Bufalo.findAll(); // Buscar todos os búfalos cadastrados
+        const ultimoBufalo = await Bufalo.findOne({ order: [['id', 'DESC']] }); // Buscar último búfalo cadastrado
 
-    Promise.all([bufalosPromise, ultimoBufaloPromise])
-        .then(([bufalos, ultimoBufalo]) => {
-            const ultimoBufaloId = ultimoBufalo ? ultimoBufalo.id : null;
+        const ultimoBufaloId = ultimoBufalo ? ultimoBufalo.id : null
 
-            // Contagem de búfalos em cada categoria
-            const totalBufalos = bufalos.length;
-            const bufalosSuplementacao = bufalos.filter(bufalo => bufalo.status === 'suplementacao').length;
-            const bufalosObservacao = bufalos.filter(bufalo => bufalo.status === 'observacao').length;
+        // Contagem de búfalos em cada categoria
+        const totalBufalos = bufalos.length;
+        const bufalosSuplementacao = bufalos.filter(bufalo => bufalo.status === 'suplementacao').length
+        const bufalosObservacao = bufalos.filter(bufalo => bufalo.status === 'observacao').length
 
-            // Renderizar o template 'zootecnico' passando todos os dados necessários
-            res.render('zootecnico', {
-                Zootecnico: Zootecnico,
-                bufalos: bufalos,
-                ultimoBufaloId: ultimoBufaloId,
-                totalBufalos: totalBufalos,
-                bufalosSuplementacao: bufalosSuplementacao,
-                bufalosObservacao: bufalosObservacao
-            });
+        // Renderizar o template 'zootecnico' passando todos os dados necessários
+        res.render('zootecnico', {
+            Zootecnico,
+            bufalos,
+            ultimoBufaloId,
+            totalBufalos,
+            bufalosSuplementacao,
+            bufalosObservacao
         })
-        .catch(error => {
-            console.error('Erro ao buscar os dados:', error);
-            res.status(500).send('Erro no servidor');
-        });
-});
+    } catch (error) {
+        console.error('Erro ao buscar os dados:', error)
+        res.status(500).send('Erro no servidor')
+    }
+})
 
-export default router;
+export default router
